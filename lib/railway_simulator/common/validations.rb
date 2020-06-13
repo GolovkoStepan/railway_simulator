231e60d1ea838634ee91b4445db988d4871440c5
s.golovko <s.golovko@fun-box.ru>
2020-06-13 11:23:24 +0500

    Разработаны модули валидации и аксессоров по ТЗ.

---
+++ lib/railway_simulator/common/validations.rb
@@ -0,0 +1,79 @@
+# frozen_string_literal: true
+
+# Custom class fields validations
+module Validation
+  VALIDATORS = {
+    presence: {
+      exp: ->(attr, _state) { !attr.to_s.empty? },
+      msg: 'Field %<attr>s must be present'
+    },
+    format: {
+      exp: ->(attr, regexp) { attr =~ regexp },
+      msg: 'Field %<attr>s have wrong format'
+    },
+    type: {
+      exp: ->(attr, klass) { attr.is_a? klass },
+      msg: 'Field %<attr>s has invalid type'
+    }
+  }.freeze
+
+  class ValidationCustomError < StandardError; end
+
+  def self.included(base)
+    base.extend ClassMethods
+    base.send :include, InstanceMethods
+  end
+
+  # Class validation functional
+  module ClassMethods
+    def validations
+      @validations ||= []
+    end
+
+    def validate(attr, options = {})
+      @validations ||= []
+
+      attr_validations = options.each_with_object([]) do |(key, value), arr|
+        next unless VALIDATORS.key? key
+
+        arr << {
+          attr: attr,
+          validator: VALIDATORS[key],
+          params: value
+        }
+      end
+
+      @validations += attr_validations
+    end
+  end
+
+  # Instance validation functional
+  module InstanceMethods
+    def errors
+      @errors ||= []
+    end
+
+    def validate!
+      @errors = []
+
+      self.class.validations.each do |validation|
+        attr       = validation[:attr]
+        msg        = validation[:validator][:msg]
+        exp        = validation[:validator][:exp]
+        params     = validation[:params]
+
+        attr_value = instance_variable_get("@#{attr}")
+        @errors << format(msg, attr: attr) unless exp.call(attr_value, params)
+      end
+
+      raise ValidationCustomError if @errors.count.positive?
+    end
+
+    def valid?
+      validate!
+      true
+    rescue ValidationCustomError
+      false
+    end
+  end
+end
